fbc78f7d245eb02d301b4107d3f06ef5324e50f6
christian <christian.froh@googlemail.com>
2020-03-29 01:06:10 +0100

    fix hospital manager create

--- lib/bed_tracking_graphql/resolver/hospital_manager.ex
+++ lib/bed_tracking_graphql/resolver/hospital_manager.ex
@@ -2,7 +2,10 @@ defmodule BedTrackingGraphql.Resolver.HospitalManager do
   use BedTrackingGraphql.Resolver.Base
   alias BedTracking.Context
 
-  def create(%{input: %{name: _, email: _, password: _, hospital_id: _} = params}, info) do
+  def create(
+        %{input: %{email: _, password: _, firstname: _, lastname: _, hospital_id: _} = params},
+        info
+      ) do
     with {:ok, _current_admin} <- Context.Authentication.current_admin(info),
          {:ok, hospital_manager} <- Context.HospitalManager.create(params) do
       {:ok, %{hospital_manager: hospital_manager}}
