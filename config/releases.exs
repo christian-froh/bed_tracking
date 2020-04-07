b48e86a0f4c10dc7236365f29a06b212ec10bc49
christian <christian.froh@googlemail.com>
2020-04-07 21:47:06 +0200

    change config

--- config/releases.exs
+++ config/releases.exs
@@ -6,4 +6,6 @@ config :bed_tracking, BedTrackingWeb.Endpoint,
   secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
   token_secret: System.fetch_env!("TOKEN_SECRET")
 
-config :bed_tracking, BedTracking.Repo, url: System.fetch_env!("DATABASE_URL")
+config :bed_tracking, BedTracking.Repo,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: System.fetch_env!("POOL_SIZE")
