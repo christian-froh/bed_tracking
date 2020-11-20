a57f30c590ca50f3187e9768b4d1e078afb18056
christian <christian.froh@googlemail.com>
2020-11-20 21:01:52 +0000

    add test

--- test/bed_tracking_graphql/hospital_test.exs
+++ test/bed_tracking_graphql/hospital_test.exs
@@ -105,83 +105,64 @@ defmodule BedTrackingGraphql.HospitalTest do
              } = response["data"]["getHospital"]["hospital"]
     end
 
-    # test "returns the latest updated_at or latest updated_by", %{token: token, hospital: hospital, hospital_manager: hospital_manager} do
-    #   hospital_manager2 = insert(:hospital_manager, hospital: hospital)
+    test "returns the latest updated_at or latest updated_by", %{token: token, hospital: hospital, hospital_manager: hospital_manager} do
+      hospital_manager2 = insert(:hospital_manager, hospital: hospital)
 
-    #   ward_amber =
-    #     insert(:ward, updated_by_hospital_manager: hospital_manager, hospital: hospital, updated_at: DateTime.add(DateTime.utc_now(), -1000, :second))
+      ward_amber =
+        insert(:ward, updated_by_hospital_manager: hospital_manager, hospital: hospital, updated_at: DateTime.add(DateTime.utc_now(), -1000, :second))
 
-    #   ward_covid = insert(:ward, updated_by_hospital_manager: hospital_manager2, hospital: hospital, updated_at: DateTime.add(DateTime.utc_now(), 0, :second))
+      ward_covid = insert(:ward, updated_by_hospital_manager: hospital_manager2, hospital: hospital, updated_at: DateTime.add(DateTime.utc_now(), 0, :second))
 
-    #   ward_amber_bed1 =
-    #     insert(:bed,
-    #       available: false,
-    #       updated_by_hospital_manager: hospital_manager2,
-    #       ward: ward_amber,
-    #       hospital: hospital,
-    #       updated_at: DateTime.add(DateTime.utc_now(), -500, :second)
-    #     )
+      insert(:bed,
+        available: false,
+        updated_by_hospital_manager: hospital_manager2,
+        ward: ward_amber,
+        hospital: hospital,
+        updated_at: DateTime.add(DateTime.utc_now(), -500, :second)
+      )
 
-    #   ward_amber_bed2 =
-    #     insert(:bed,
-    #       available: false,
-    #       updated_by_hospital_manager: hospital_manager,
-    #       ward: ward_amber,
-    #       hospital: hospital,
-    #       updated_at: DateTime.add(DateTime.utc_now(), -200, :second)
-    #     )
+      ward_amber_bed2 =
+        insert(:bed,
+          available: false,
+          updated_by_hospital_manager: hospital_manager,
+          ward: ward_amber,
+          hospital: hospital,
+          updated_at: DateTime.add(DateTime.utc_now(), -200, :second)
+        )
 
-    #   ward_covid_bed1 =
-    #     insert(:bed,
-    #       available: false,
-    #       updated_by_hospital_manager: hospital_manager,
-    #       ward: ward_covid,
-    #       hospital: hospital,
-    #       updated_at: DateTime.add(DateTime.utc_now(), -200, :second)
-    #     )
+      insert(:bed,
+        available: false,
+        updated_by_hospital_manager: hospital_manager,
+        ward: ward_covid,
+        hospital: hospital,
+        updated_at: DateTime.add(DateTime.utc_now(), -200, :second)
+      )
 
-    #   ward_covid_bed2 =
-    #     insert(:bed,
-    #       available: false,
-    #       updated_by_hospital_manager: hospital_manager2,
-    #       ward: ward_covid,
-    #       hospital: hospital,
-    #       updated_at: DateTime.add(DateTime.utc_now(), -500, :second)
-    #     )
+      insert(:bed,
+        available: false,
+        updated_by_hospital_manager: hospital_manager2,
+        ward: ward_covid,
+        hospital: hospital,
+        updated_at: DateTime.add(DateTime.utc_now(), -500, :second)
+      )
 
-    #   response =
-    #     graphql_query(query: @query, token: token)
-    #     |> BedTrackingWeb.Endpoint.call([])
-    #     |> Map.get(:resp_body)
-    #     |> Jason.decode!()
+      response =
+        graphql_query(query: @query, token: token)
+        |> BedTrackingWeb.Endpoint.call([])
+        |> Map.get(:resp_body)
+        |> Jason.decode!()
 
-    #   assert [
-    #            %{
-    #              "id" => "f182de48-7153-4a88-b286-328dc42e61b2",
-    #              "lastUpdatedAtOfWardOrBeds" => "2020-11-20T20:09:00.657249Z",
-    #              "lastUpdatedByHospitalManagerOfWardOrBeds" => %{"id" => "dfc1c0f6-2328-4ff8-9b2f-1db55d5538e8"}
-    #            },
-    #            %{
-    #              "id" => "8d2709d2-74c3-420c-8daa-1e05a6d9c14e",
-    #              "lastUpdatedAtOfWardOrBeds" => "2020-11-20T20:09:00.692500Z",
-    #              "lastUpdatedByHospitalManagerOfWardOrBeds" => %{"id" => "ad4349b1-79c7-4cf8-8d3d-af00a2614260"}
-    #            },
-    #            %{
-    #              "id" => "9c95894f-3e70-4a21-b80d-5ce8c63b7f20",
-    #              "lastUpdatedAtOfWardOrBeds" => "2020-11-20T20:09:00.715293Z",
-    #              "lastUpdatedByHospitalManagerOfWardOrBeds" => %{"id" => "c6c269ad-f8ab-4e64-94b3-8da9ba5f7e81"}
-    #            },
-    #            %{
-    #              "id" => "08b41e26-4777-44fb-8efa-df8a2b7129c9",
-    #              "lastUpdatedAtOfWardOrBeds" => "2020-11-20T20:05:41.005679Z",
-    #              "lastUpdatedByHospitalManagerOfWardOrBeds" => %{"id" => "d0f30449-253e-482b-9656-9e65b639d2aa"}
-    #            },
-    #            %{
-    #              "id" => "bf7dc76f-cb30-493b-b8cd-e04aac16677f",
-    #              "lastUpdatedAtOfWardOrBeds" => "2020-11-20T20:09:00.995776Z",
-    #              "lastUpdatedByHospitalManagerOfWardOrBeds" => %{"id" => "44d9ec03-24ab-496d-9554-61d72ac3a2ba"}
-    #            }
-    #          ] = response["data"]["getHospital"]["hospital"]["wards"]
-    # end
+      wards_result = response["data"]["getHospital"]["hospital"]["wards"]
+
+      ward_covid_result = Enum.find(wards_result, fn %{"id" => id} -> id == ward_covid.id end)
+      {:ok, datetime, 0} = DateTime.from_iso8601(ward_covid_result["lastUpdatedAtOfWardOrBeds"])
+      assert datetime == ward_covid.updated_at
+      assert ward_covid_result["lastUpdatedByHospitalManagerOfWardOrBeds"]["id"] == hospital_manager2.id
+
+      ward_amber_result = Enum.find(wards_result, fn %{"id" => id} -> id == ward_amber.id end)
+      {:ok, datetime, 0} = DateTime.from_iso8601(ward_amber_result["lastUpdatedAtOfWardOrBeds"])
+      assert datetime == ward_amber_bed2.updated_at
+      assert ward_amber_result["lastUpdatedByHospitalManagerOfWardOrBeds"]["id"] == hospital_manager.id
+    end
   end
 end
